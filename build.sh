633c15c20843bdad3811a00593013eff89bb26ac
sqma <masiqi@westlake.edu.cn>
2024-10-15 21:58:34 +0800

    add compare api update requirements

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # develop mode
 # python setup.py develop
+
 # build whl
-# python -m build --wheel
+python -m build --wheel
+
 # upload to testpypi
-twine upload -r testpypi dist/dmtev_learn-0.0.4-py3-none-any.whl --verbose
+twine upload -r testpypi dist/dmtev_learn-0.0.5-py3-none-any.whl --verbose
--- dmtev/dmtev_.py
+++ dmtev/dmtev_.py
@@ -1,6 +1,7 @@
 from sklearn.base import BaseEstimator
 from sklearn.utils import check_random_state, check_array
 from sklearn.utils.validation import check_is_fitted
+from sklearn.manifold import TSNE
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import normalize
 
@@ -11,6 +12,7 @@ from os import PathLike
 from pytorch_lightning import Trainer
 import torch
 import logging
+import umap
 
 from .LitPatNN_ import LitPatNN
 
@@ -22,6 +24,9 @@ class DMTEV(BaseEstimator):
                  device_id:int|None=None,
                  checkpoint_path: PathLike|None="./",
                  **kwargs) -> None:
+        '''
+        
+        '''
         super().__init__()
         seed_everything(seed)
         os.makedirs(checkpoint_path, exist_ok=True)
@@ -37,7 +42,6 @@ class DMTEV(BaseEstimator):
                 max_epochs=epochs,
                 logger=False,
                 default_root_dir=checkpoint_path,
-                checkpoint_callback=False,
                 enable_checkpointing=False,
             )
             device = torch.device(f"cuda:{device_id}")
@@ -67,7 +71,6 @@ class DMTEV(BaseEstimator):
         self.trainer.fit(self.model)
         return self.model.ins_emb
 
-    # 暂不支持
     def fit(self, X, y=None):
         if not isinstance(X, np.ndarray) and not isinstance(X, torch.Tensor):
             raise ValueError("X must be a numpy array or a torch tensor")
@@ -75,8 +78,44 @@ class DMTEV(BaseEstimator):
         self.model.adapt(X)
         self.trainer.fit(self.model)
         
-
-    # 暂不支持
     def transform(self, X):
         _, _, lat3 = self.model(X)
         return lat3.cpu().detach().numpy()
+    
+    def compare(self, X, plot=None):
+        '''
+        Compare the embeddings of DMT-EV, UMAP and TSNE
+        X: np.ndarray or torch.Tensor
+        '''
+                
+        if not isinstance(X, np.ndarray) and not isinstance(X, torch.Tensor):
+            raise ValueError("X must be a numpy array or a torch tensor")
+
+        logging.debug("Start DMT-EV")
+        dmt_embedding = self.fit_transform(X)
+        logging.debug("Start UMAP")
+        umap_embedding = umap.UMAP().fit_transform(X)
+        logging.debug("Start TSNE")
+        tsne_embedding = TSNE().fit_transform(X)
+        
+        if plot:
+            logging.debug("Plotting")
+            if not isinstance(plot, str):
+                raise ValueError("plot must be a path")
+            if not os.path.exists(os.path.dirname(plot)):
+                os.makedirs(os.path.dirname(plot))
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+            ax[0].scatter(dmt_embedding[:, 0], dmt_embedding[:, 1])
+            ax[0].set_title("DMTEV")
+            ax[1].scatter(umap_embedding[:, 0], umap_embedding[:, 1])
+            ax[1].set_title("UMAP")
+            ax[2].scatter(tsne_embedding[:, 0], tsne_embedding[:, 1])
+            ax[2].set_title("TSNE")
+            plt.savefig(plot)
+
+        return {
+            "dmt_embedding": dmt_embedding,
+            "umap_embedding": umap_embedding,
+            "tsne_embedding": tsne_embedding
+        }
